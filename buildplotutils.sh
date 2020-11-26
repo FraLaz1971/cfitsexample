e77e22f190c2d2cd0329dc6e30442877f8d00ac4
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-26 18:50:29 +0100

    updated buildplotutils.sh

--- buildplotutils.sh
+++ buildplotutils.sh
@@ -1,9 +1,7 @@
-bash buildlibpng.sh
 LIBPNGDIR=src/libpng-1.6.37
 LIBZDIR=src/libpng-1.6.37/zlib-1.2.11
 PUDIR=src/plotutils
 # build plotutils
-cd $PUDIR
 if   [[ "$OS" == "Windows_NT" ]]
 then
         echo "detected Microsoft $OS $(uname)"
@@ -11,13 +9,18 @@ then
         if [[ $arch == i686 ]]
         then
                 echo 'ex. build-plotutils-win-mingw-i686.sh'
-		configure-win-x86_64.sh
+		bash buildlibpng-win-mingw-x86-64.sh
+		cd $PUDIR
+		bash configure-win-x86_64.sh
 		mingw32-make
 		mingw32-make install
         elif [[ $arch == x86_64 ]]
         then
                 echo 'ex. build-plotutils-win-mingw-x86-64.sh'
-		configure-win-i686.sh
+		bash buildlibpng-win-mingw-i686.sh
+		cd $PUDIR
+		bash configure-win-i686.sh
+		mingw32-make -j4
 		mingw32-make install
         else
                 echo "unhandled arch"
@@ -27,14 +30,16 @@ then
         	echo "detected GNU/linux $OS"
         	uname -a
         	lsb_release -a
-		. configure-linux.sh
-		make
+		bash buildlibpng.sh
+		cd $PUDIR
+		bash configure-linux.sh
+		make -j4
 		make install
 	elif [[ $(uname) == "Darwin" ]]
 	then
         	echo "detected Apple $OS $(uname)"
         	. configure-mac.sh
-		make
+		make -j4
 		make install
 	else
         	echo "unknow OS"
