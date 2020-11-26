c4c077e1144608ecad1f3b75141f749d2008bfc8
unknown <fralaz1971@egmail.com>
2020-11-26 18:03:28 +0100

    tested on win mingw i686

--- cleanlibpng.sh
+++ cleanlibpng.sh
@@ -1,13 +1,13 @@
 LIBPNGDIR=src/libpng-1.6.37
-cd $LIBPNGDIR
-# build zlib
 LIBZDIR=src/libpng-1.6.37/zlib-1.2.11
 cd $LIBZDIR
-ZROOTDIR=$(pwd);
 make clean
+make -f win32/Makefile.i686-w64-mingw32-gcc clean
+make -f win32/Makefile.x86_64-w64-mingw32-gcc clean
 rm -f *.log
-# build libpng
-PNGROOTDIR=$(pwd);
+cd $OLDPWD
+cd $LIBPNGDIR
+# clean libpng
 make clean
 rm -f *.log
 cd $OLDPWD
